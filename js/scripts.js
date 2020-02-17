c3e05dd6c81a88c0bd9dc78583083322547b8313
Kanashi-Senshi <mwashamichael@gmail.com>
2020-02-17 10:01:29 +0300

    Adds a description of birthday date in output

--- js/scripts.js
+++ js/scripts.js
@@ -14,35 +14,13 @@ function akanName(){
     var date = parseInt(date);
     var year = new Date (`${year}-${month}-${date}`);
     var d = year.getDay();
-    console.log("New date is " + d);
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-    console.log(name + " your Akan name is " + maleName[d])
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     if (document.getElementById('male').checked) {
-        
-    }
-    else if (document.getElementById('female').checked) {
-        if (d === 0) {
-            document.getElementById("output").innerHTML = name + " your Akan name is AKosua";
-        }
-        else if (d === 1) {
-            document.getElementById("output").innerHTML = name + " your Akan name is Adwoa";
-        }
-        else if (d === 2) {
-            document.getElementById("output").innerHTML = name + " your Akan name is Abenaa";
-        }
-        else if (d === 3) {
-            document.getElementById("output").innerHTML = name + " your Akan name is Akua";
-        }
-        else if (d === 4) {
-            document.getElementById("output").innerHTML = name + " your Akan name is Yaa";
-        }
-        else if (d === 5) {
-            document.getElementById("output").innerHTML = name + " your Akan name is Afua";
-        }
-        else {
-            document.getElementById("output").innerHTML = name + " your Akan name is Ama";
-        }
+        document.getElementById("output").innerHTML =( name + " your Akan name is " + maleName[d] + " and you were born on a " + weekday[d] + ".");
+    }else if (document.getElementById('female').checked) {
+        document.getElementById("output").innerHTML =( name + " your Akan name is " + femaleName[d] + " and you were born on a " + weekday[d] + ".");
     }else{
         alert("Choose a gender.")
     }
