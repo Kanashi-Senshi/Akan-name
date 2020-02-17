2e8588079f3186d2968887ac52049e28ad1e6ca2
Kanashi-Senshi <mwashamichael@gmail.com>
2020-02-17 11:39:59 +0300

    Removes get date function and adds formula

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,25 @@ function akanName(){
     var birthday = document.getElementById("personBirthday").value;
     console.log(birthday)
     var birthday = Array.from(birthday);
-    var year =birthday.slice(0,4);
+    var CC =birthday.slice(2,4);
+    var YY = birthday.slice(0,2)
     var month =birthday.slice(5,7);
     var date =birthday.slice(8,10);
-    var year = year.join('')
+    var CC = CC.join ('');
+    var YY = YY.join ('');
     var month = month.join('');
     var date = date.join('');
-    var year = parseInt(year);
-    var month = parseInt(month);
-    var date = parseInt(date);
-    var year = new Date (`${year}-${month}-${date}`);
-    var d = year.getDay();
-    console.log(d);
+    var YY = parseInt(YY);
+    var CC = parseInt(CC);
+    var MM= parseInt(month);
+    var DD = parseInt(date);
+    var d = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+    d = Math.floor(d);
+    d= Math.abs(d);
     var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
     var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
     var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    if (d ===d)
+    if (d === d)
         if (document.getElementById('male').checked) {
             document.getElementById("output").innerHTML =( name + " your Akan name is " + maleName[d] + " and you were born on a " + weekday[d] + ".");
         }else if (document.getElementById('female').checked) {
